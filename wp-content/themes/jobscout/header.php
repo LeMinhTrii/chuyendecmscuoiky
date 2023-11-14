92ed695d114f24534e34a2f43d88872a7c20d8c2
Le Minh Tri <leminhtri@MacBook-Air.local>
2023-11-15 02:04:10 +0700

    updatebannersearch

--- wp-content/themes/jobscout/header.php
+++ wp-content/themes/jobscout/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -8,55 +9,59 @@
  *
  * @package JobScout
  */
-    /**
-     * Doctype Hook
-     * 
-     * @hooked jobscout_doctype
-    */
-    do_action( 'jobscout_doctype' );
+/**
+ * Doctype Hook
+ * 
+ * @hooked jobscout_doctype
+ */
+do_action('jobscout_doctype');
 ?>
+
 <head itemscope itemtype="https://schema.org/WebSite">
-	<?php 
+    <?php
     /**
      * Before wp_head
      * 
      * @hooked jobscout_head
-    */
-    do_action( 'jobscout_before_wp_head' );
-    
+     */
+    do_action('jobscout_before_wp_head');
+
     wp_head(); ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,700,800|Open+Sans+Condensed:300,700|Prata&subset=vietnamese' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/build/style-editor-customizer.css" media="all">
 </head>
 
 <body <?php body_class(); ?> itemscope itemtype="https://schema.org/WebPage">
 
-<?php
+    <?php
     wp_body_open();
-    
+
     /**
      * Before Header
      * @hooked jobscout_responsive_header - 15
      * @hooked jobscout_page_start - 20 
-    */
-    do_action( 'jobscout_before_header' );
-    
+     */
+    do_action('jobscout_before_header');
+
     /**
      * Header
      * 
      * @hooked jobscout_header - 20     
-    */
-    do_action( 'jobscout_header' );
+     */
+    do_action('jobscout_header');
 
     /**
      * Content
      * 
      * @hooked jobscout_breadcrumbs_bar
-    */
-    do_action( 'jobscout_after_header' );
-    
-    
+     */
+    do_action('jobscout_after_header');
+
+
     /**
      * Content
      * 
      * @hooked jobscout_content_start
-    */
-    do_action( 'jobscout_content' );
+     */
+    do_action('jobscout_content');
--- wp-content/themes/jobscout/template-parts/header-form.php
+++ wp-content/themes/jobscout/template-parts/header-form.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * Creating a custom job search form for homepage
@@ -8,48 +9,70 @@
  *
  * @package JobScout
  */
-$find_a_job_link = get_option( 'job_manager_jobs_page_id', 0 );
-$post_slug       = get_post_field( 'post_name', $find_a_job_link );
-$ed_job_category = get_option( 'job_manager_enable_categories' );  
+$find_a_job_link = get_option('job_manager_jobs_page_id', 0);
+$post_slug       = get_post_field('post_name', $find_a_job_link);
+$ed_job_category = get_option('job_manager_enable_categories');
 
-if( $post_slug ){
-    $action_page =  home_url( '/'. $post_slug );
-}else {
-    $action_page =  home_url( '/' );
+if ($post_slug) {
+  // $action_page =  home_url('/' . $post_slug);
+  $action_page =  home_url('/');
+} else {
+  $action_page =  home_url('/');
 }
 ?>
 
+
 <div class="job_listings">
 
-  <form class="jobscout_job_filters" method="GET" action="<?php echo esc_url( $action_page ) ?>">
-    <div class="search_jobs">
+  <form class="jobscout_job_filters" method="GET" action="<?php echo esc_url($action_page) ?>">
+    <div class="custom_search_job">
+      <div class="search_jobs">
+        <div class="wrap">
+          <div class="search_keywords">
+            <label for="search_keywords"><?php esc_html_e('Keywords', 'jobscout'); ?></label>
+            <input type="text" id="search_keywords" name="search_keywords" placeholder="<?php esc_attr_e('Keywords', 'jobscout'); ?>">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+          <!-- get location by job -->
+          <?php
+          global $wpdb;
+          $query = "
+            SELECT DISTINCT meta_location.meta_value AS job_location
+            FROM {$wpdb->posts} AS wp_posts
+            LEFT JOIN {$wpdb->postmeta} AS meta_location ON wp_posts.ID = meta_location.post_id AND meta_location.meta_key = '_job_location'
+            WHERE wp_posts.post_type = 'job_listing' AND wp_posts.post_status = 'publish';
+        ";
+          $results = $wpdb->get_results($query);
+          ?>
+          <div class="search_location">
+            <label for="search_location"><?php esc_html_e('Location', 'jobscout'); ?></label>
+            <select id="search_location" name="search_location">
+              <option value=""><?php esc_html_e('All Locations', 'jobscout'); ?></option>
+              <?php foreach ($results as $location) : ?>
+                <option><?php echo esc_html($location->job_location); ?></option>
+              <?php endforeach; ?>
+            </select>
+            <i class="fa-solid fa-location-dot"></i>
+          </div>
+          <div class="custom_search-btn">
+            <div class="search_submit">
+              <input type="submit" value="<?php esc_attr_e('Search job', 'jobscout'); ?>" />
+            </div>
+          </div>
+        </div>
 
-      <div class="search_keywords">
-        <label for="search_keywords"><?php esc_html_e( 'Keywords', 'jobscout' ); ?></label>
-        <input type="text" id="search_keywords" name="search_keywords" placeholder="<?php esc_attr_e( 'Keywords', 'jobscout' ); ?>">
-      </div>
-
-      <div class="search_location">
-        <label for="search_location"><?php esc_html_e( 'Location', 'jobscout' ); ?></label>
-        <input type="text"  id="search_location" name="search_location" placeholder="<?php esc_attr_e( 'Location', 'jobscout' ); ?>">
-      </div>
-      
-      <?php if( $ed_job_category ){ ?>
+        <?php if ($ed_job_category) { ?>
           <div class="search_categories custom_search_categories">
-            <label for="search_category"><?php esc_html_e( 'Job Category', 'jobscout' ); ?></label>
+            <label for="search_category"><?php esc_html_e('Job Category', 'jobscout'); ?></label>
             <select id="search_category" class="robo-search-category" name="search_category">
-            <option value=""><?php _e( 'Select Job Category', 'jobscout' ); ?></option>
-              <?php foreach ( get_job_listing_categories() as $jobcat ) : ?>
-                <option value="<?php echo esc_attr( $jobcat->term_id ); ?>"><?php echo esc_html( $jobcat->name ); ?></option>
+              <option value=""><?php _e('Select Job Category', 'jobscout'); ?></option>
+              <?php foreach (get_job_listing_categories() as $jobcat) : ?>
+                <option value="<?php echo esc_attr($jobcat->term_id); ?>"><?php echo esc_html($jobcat->name); ?></option>
               <?php endforeach; ?>
             </select>
           </div>
-      <?php } ?>
-      
-      <div class="search_submit">
-        <input type="submit" value="<?php esc_attr_e( 'Search', 'jobscout'); ?>" />
+        <?php } ?>
       </div>
-
     </div>
   </form>
 
