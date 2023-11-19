069cb8b07d48ab14930801b11cf44932d83c2930
NguyenKien <dacnguyen732003@gmail.com>
2023-11-20 01:00:51 +0700

    ver1

--- wp-content/themes/jobscout/sections/client.php
+++ wp-content/themes/jobscout/sections/client.php
@@ -1,14 +1,16 @@
 <?php
+
 /**
  * Client Section
  * 
  * @package JobScout
  */
 
-if( is_active_sidebar( 'client' ) ){ ?>
+if (is_active_sidebar('client')) { ?>
+	<script src="https://kit.fontawesome.com/cbeaa37f05.js" crossorigin="anonymous"></script>
 	<section id="client-section" class="client-section">
 		<div class="container">
-	    	<?php dynamic_sidebar( 'client' ); ?>
+			<?php dynamic_sidebar('client'); ?>
 		</div>
 	</section> <!-- .bg-cta-section -->
 <?php
